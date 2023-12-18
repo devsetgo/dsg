bf5e90eda6c2ba8eb9e0a787c644649e6487b513
devsetgo <mikeryan56@gmail.com>
2023-12-18 03:10:41 +0000

    working on new base

--- src/database_tables.py
+++ src/database_tables.py
@@ -21,6 +21,15 @@ class User(base_schema.SchemaBase, async_db.Base):
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
 
+    # Define the child relationship to the InterestingThings class
+    InterestingThings = relationship(
+        "InterestingThings", back_populates="user"
+    )  # Relationship to the InterestingThings class
+    # Define the child relationship to the Categories class
+    Categories = relationship(
+        "Categories", back_populates="user"
+    )  # Relationship to the Categories class
+
 
 class InterestingThings(base_schema.SchemaBase, async_db.Base):
     __tablename__ = "interesting_things"  # Name of the table in the database
@@ -29,17 +38,13 @@ class InterestingThings(base_schema.SchemaBase, async_db.Base):
     name = Column(String, unique=False, index=True)  # name of item
     description = Column(String, unique=False, index=True)  # description of item
     url = Column(String, unique=False, index=True)  # url of item
+    category = Column(String, unique=False, index=True)  # category of item
+    # Define the parent relationship to the User class
+    user_id = Column(Integer, ForeignKey("users.pkid"))  # Foreign key to the User table
+    user = relationship(
+        "User", back_populates="InterestingThings"
+    )  # Relationship to the User class
 
-#     # Define the parent relationship to the User class
-#     user_id = Column(Integer, ForeignKey("users.pkid"))  # Foreign key to the User table
-#     user = relationship(
-#         "User", back_populates="InterestingThings"
-#     )  # Relationship to the User class
-#     category_id = Column(Integer, ForeignKey("categories.pkid"))  # Foreign key to the Categories table
-#     categories = relationship(
-#         "Categories", back_populates="InterestingThings"
-#     )  # Relationship to the Categories class
-    
 
 class Categories(base_schema.SchemaBase, async_db.Base):
     __tablename__ = "categories"  # Name of the table in the database
@@ -49,14 +54,8 @@ class Categories(base_schema.SchemaBase, async_db.Base):
     description = Column(String, unique=False, index=True)  # description of item
     is_system = Column(Boolean, default=True)  # If the category is a system default
     is_active = Column(Boolean, default=True)  # If the category is active
-#     # Define the parent relationship to the User class
-#     user_id = Column(Integer, ForeignKey("users.pkid"))  # Foreign key to the User table
-#     user = relationship(
-#         "User", back_populates="Categories"
-#     )  # Relationship to the User class
-#     # Define the parent relationship to the Categories class
-#     category_id = Column(Integer, ForeignKey("categories.pkid"))  # Foreign key to the Categories table
-#     Categories = relationship(
-#         "Categories", back_populates="Categories"
-#     )  # Relationship to the Categories class
-
+    # Define the parent relationship to the User class
+    user_id = Column(Integer, ForeignKey("users.pkid"))  # Foreign key to the User table
+    user = relationship(
+        "User", back_populates="Categories"
+    )  # Relationship to the User class
