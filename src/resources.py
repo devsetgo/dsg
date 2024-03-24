bca4d9d9489f67c240f8603dd87f92246ab34556
devsetgo <mikeryan56@gmail.com>
2024-03-24 02:29:26 +0000

    working on better setup for demo notes

--- src/resources.py
+++ src/resources.py
@@ -208,13 +208,15 @@ async def add_notes(user_id: str, qty_notes: int = settings.create_demo_notes_qt
     demo_notes = []
 
     for _ in tqdm(range(qty_notes)):
+        
         mood = random.choice(moods)
-        note = silly.paragraph()
+        length = random.randint(1,20)
+        note = silly.paragraph(length=length)
         summary = note[:50]
         tags = list(set([silly.adjective() for x in range(1, 4)]))
 
-        # Generate a random date within the last 3 years
-        days_in_three_years = 365 * 2
+        # Generate a random date within the last X years
+        days_in_three_years = 365 * 15
         random_number_of_days = random.randrange(days_in_three_years)
         date_created = datetime.now() - timedelta(days=random_number_of_days)
 
