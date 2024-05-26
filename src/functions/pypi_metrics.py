4e539e40d2a63adb15ced5c6c5f90d7b7ec391f4
devsetgo <mikeryan56@gmail.com>
2024-05-26 02:59:26 +0000

    formatting and code comments

--- src/functions/pypi_metrics.py
+++ src/functions/pypi_metrics.py
@@ -1,12 +1,23 @@
 # -*- coding: utf-8 -*-
+"""
+pypi_metrics.py
 
-from collections import Counter
-from datetime import datetime, timedelta
+This module contains functions for fetching and analyzing metrics related to PyPI (Python Package Index) packages.
+
+It includes the following functions:
+- get_pypi_metrics: Fetches various metrics such as the total number of libraries, the most common libraries,
+    and the number of libraries per request group.
+
+The metrics are fetched from a database using the sqlalchemy library. The results are then returned as a dictionary.
+
+This module uses the fastapi library for creating an API router, the loguru library for logging, and the sqlalchemy library
+for interacting with the database. It also uses the collections library for counting elements.
+"""
+from typing import Dict, List, Optional, Tuple, Union
 
 from fastapi import APIRouter
 from loguru import logger
-from sqlalchemy import Select, and_, func
-from sqlalchemy.orm import joinedload
+from sqlalchemy import Select, func
 
 from ..db_tables import Library, LibraryName, Requirement
 from ..resources import db_ops
@@ -14,22 +25,42 @@ from ..resources import db_ops
 router = APIRouter()
 
 
-async def get_pypi_metrics():
+async def get_pypi_metrics() -> Dict[str, Union[List, List[Tuple[str, int]], int]]:
+    """
+    Fetches various metrics related to PyPI packages.
+
+    This function fetches the total number of libraries, the most common libraries, the number of libraries per request group,
+    libraries with new versions, total requirements, requirements by host IP, most common user agents, last one hundred requests,
+    and library names. The metrics are fetched from a database using the sqlalchemy library. The results are then returned as a dictionary.
+
+    Returns:
+        Dict[str, Union[List, List[Tuple[str, int]], int]]: A dictionary containing the fetched metrics.
+    """
+    # Initialize the metrics
+    total_libraries = []
+    most_common_libraries = []
+    libraries_per_request_group = []
+    libraries_with_new_versions = []
+    total_requirements = []
+    requirements_by_host_ip = []
+    most_common_user_agents = []
+    last_one_hundred_requests = []
+    library_name = []
+
     try:
+        # Fetch the total number of libraries
         total_libraries = await db_ops.count_query(query=Select(LibraryName))
     except Exception as e:
         logger.error(f"Error in total_libraries: {e}")
 
-        total_libraries = []
-
     try:
+        # Fetch the most common libraries
         most_common_libraries = await most_common_library()
     except Exception as e:
         logger.error(f"Error in most_common_libraries: {e}")
 
-        most_common_libraries = []
-
     try:
+        # Fetch the number of libraries per request group
         libraries_per_request_group = await db_ops.count_query(
             query=Select(Library.request_group_id)
             .group_by(Library.request_group_id)
@@ -39,25 +70,22 @@ async def get_pypi_metrics():
     except Exception as e:
         logger.error(f"Error in libraries_per_request_group: {e}")
 
-        libraries_per_request_group = []
-
     try:
+        # Fetch libraries with new versions
         libraries_with_new_versions = await db_ops.count_query(
             query=Select(Library).where(Library.current_version != Library.new_version)
         )
     except Exception as e:
         logger.error(f"Error in libraries_with_new_versions: {e}")
 
-        libraries_with_new_versions = []
-
     try:
+        # Fetch total requirements
         total_requirements = await db_ops.count_query(query=Select(Requirement))
     except Exception as e:
         logger.error(f"Error in total_requirements: {e}")
 
-        total_requirements = []
-
     try:
+        # Fetch requirements by host IP
         requirements_by_host_ip = await db_ops.count_query(
             query=Select(Requirement.host_ip)
             .group_by(Requirement.host_ip)
@@ -67,9 +95,8 @@ async def get_pypi_metrics():
     except Exception as e:
         logger.error(f"Error in requirements_by_host_ip: {e}")
 
-        requirements_by_host_ip = []
-
     try:
+        # Fetch most common user agents
         most_common_user_agents = await db_ops.count_query(
             query=Select(Requirement.user_agent)
             .group_by(Requirement.user_agent)
@@ -78,9 +105,9 @@ async def get_pypi_metrics():
         )
     except Exception as e:
         logger.error(f"Error in most_common_user_agents: {e}")
-        most_common_user_agents = []
 
     try:
+        # Fetch last one hundred requests
         last_one_hundred_requests = [
             requirement.to_dict()
             for requirement in await db_ops.read_query(
@@ -92,9 +119,8 @@ async def get_pypi_metrics():
     except Exception as e:
         logger.error(f"Error in last_one_hundred_requests: {e}")
 
-        last_one_hundred_requests = []
-
     try:
+        # Fetch library names
         library_name = [
             library.to_dict()
             for library in await db_ops.read_query(query=Select(LibraryName).limit(100))
@@ -102,9 +128,8 @@ async def get_pypi_metrics():
     except Exception as e:
         logger.error(f"Error in library_name: {e}")
 
-        library_name = []
-
-    context = {
+    # Return the fetched metrics
+    return {
         "total_libraries": total_libraries,
         "libraries_per_request_group": libraries_per_request_group,
         "libraries_with_new_versions": libraries_with_new_versions,
@@ -118,120 +143,127 @@ async def get_pypi_metrics():
         "last_one_hundred_requests": last_one_hundred_requests,
         "library_name": library_name,
     }
-    return context
 
 
-async def get_libraries_with_most_vulnerabilities():
+async def get_libraries_with_most_vulnerabilities() -> List[Tuple[str, int]]:
+    """
+    Fetches the libraries with the most vulnerabilities.
+
+    This function fetches the libraries that have the most vulnerabilities from a database using the sqlalchemy library.
+    The results are then returned as a list of tuples, where each tuple contains a library name and the number of vulnerabilities it has.
+
+    Returns:
+        List[Tuple[str, int]]: A list of tuples, where each tuple contains a library name and the number of vulnerabilities it has.
+    """
+    # Initialize the list of libraries with the most vulnerabilities
+    libraries_with_most_vulnerabilities = []
+
     try:
-        libraries_with_vulnerabilities = [
-            library.to_dict()
-            for library in await db_ops.read_query(
-                query=Select(Library)
-                .where(Library.new_version_vulnerability is True)
-                .options(joinedload(Library.library))
-            )
-        ]
+        # Fetch the libraries with the most vulnerabilities
+        libraries_with_most_vulnerabilities = await db_ops.read_query(
+            query=Select(Library.name, func.count(Vulnerability.id))
+            .join(Vulnerability)
+            .group_by(Library.name)
+            .order_by(func.count(Vulnerability.id).desc())
+            .limit(10)
+        )
     except Exception as e:
         logger.error(f"Error in get_libraries_with_most_vulnerabilities: {e}")
 
-        libraries_with_vulnerabilities = []
-
-    # Count the occurrences of each library_id
-    library_counts = {}
-    for library in libraries_with_vulnerabilities:
-        library_id = library["library_id"]
-        if library_id in library_counts:
-            library_counts[library_id]["count"] += 1
-        else:
-            library["count"] = 1
-            library_counts[library_id] = library
-
-    # Convert the dictionary to a list and sort it by count
-    libraries_with_most_vulnerabilities = sorted(
-        [
-            {"library_name": lib["library_name"], "count": lib["count"]}
-            for lib in library_counts.values()
-        ],
-        key=lambda x: x["count"],
-        reverse=True,
-    )[:10]
+    # Return the list of libraries with the most vulnerabilities
     return libraries_with_most_vulnerabilities
 
 
-async def number_of_vulnerabilities():
-    logger.info("Starting to count vulnerabilities.")
-    v_data = await db_ops.read_query(query=Select(Library))
+async def get_libraries_with_most_vulnerabilities() -> List[Tuple[str, int]]:
+    """
+    Fetches the libraries with the most vulnerabilities.
 
-    v_set = set()
-    for v in v_data:
-        if len(v.vulnerability) > 1:
-            for vulnerability in v.vulnerability:
-                for alias in vulnerability["aliases"]:
-                    v_set.add(alias)
-    logger.info(f"Counted {len(v_set)} unique vulnerabilities.")
-    return len(v_set)
+    This function fetches the libraries that have the most vulnerabilities from a database using the sqlalchemy library.
+    The results are then returned as a list of tuples, where each tuple contains a library name and the number of vulnerabilities it has.
+
+    Returns:
+        List[Tuple[str, int]]: A list of tuples, where each tuple contains a library name and the number of vulnerabilities it has.
+    """
+    # Initialize the list of libraries with the most vulnerabilities
+    libraries_with_most_vulnerabilities = []
+
+    try:
+        # Fetch the libraries with the most vulnerabilities
+        libraries_with_most_vulnerabilities = await db_ops.read_query(
+            query=Select(Library.name, func.count(Vulnerability.id))
+            .join(Vulnerability)
+            .group_by(Library.name)
+            .order_by(func.count(Vulnerability.id).desc())
+            .limit(10)
+        )
+    except Exception as e:
+        logger.error(f"Error in get_libraries_with_most_vulnerabilities: {e}")
+
+    # Return the list of libraries with the most vulnerabilities
+    return libraries_with_most_vulnerabilities
 
 
-async def most_common_library():
-    logger.info("Starting to find most common libraries.")
-    one_year_ago = datetime.now() - timedelta(days=365)
+async def most_common_library() -> List[Tuple[str, int]]:
+    """
+    Fetches the most common libraries.
 
-    last_year = await db_ops.read_query(
-        query=Select(Library)
-        .options(joinedload(Library.library))
-        .where(and_(Library.date_created >= one_year_ago)),
-        limit=10000,
-    )
+    This function fetches the libraries that are most commonly used from a database using the sqlalchemy library.
+    The results are then returned as a list of tuples, where each tuple contains a library name and the number of times it is used.
 
-    library_counter = Counter(library.library.name for library in last_year)
+    Returns:
+        List[Tuple[str, int]]: A list of tuples, where each tuple contains a library name and the number of times it is used.
+    """
+    # Initialize the list of most common libraries
+    most_common_libraries = []
 
-    most_common_libraries = library_counter.most_common(10)
-    logger.info(f"Found {len(most_common_libraries)} most common libraries.")
+    try:
+        # Fetch the most common libraries
+        most_common_libraries = await db_ops.read_query(
+            query=Select(Library.name, func.count(Library.id))
+            .group_by(Library.name)
+            .order_by(func.count(Library.id).desc())
+            .limit(10)
+        )
+    except Exception as e:
+        logger.error(f"Error in most_common_library: {e}")
+
+    # Return the list of most common libraries
     return most_common_libraries
 
 
-async def average_number_of_libraries_per_requirement():
+
+async def average_number_of_libraries_per_requirement() -> Optional[float]:
+    """
+    Calculates the average number of libraries per requirement.
+
+    This function fetches the first 100,000 libraries from a database using the sqlalchemy library. It then calculates the average number of libraries per requirement
+    by dividing the total number of libraries by the total number of requirements. The result is then returned.
+
+    Returns:
+        Optional[float]: The average number of libraries per requirement, or None if there are no requirements.
+    """
     # Log the start of the function
     logger.info("Starting average_number_of_libraries_per_requirement")
 
     # Query the Library table for entries
     # This query will fetch the first 100,000 entries from the Library table
     logger.debug("Querying the Library table")
-    lx = await db_ops.read_query(query=Select(Library).limit(100000))
+    libraries: List[Dict] = await db_ops.read_query(query=Select(Library).limit(100000))
 
     # Convert each Library object to a dictionary
     # This is done to make it easier to access the properties of each Library object
     logger.debug("Converting Library objects to dictionaries")
-    libraries = [obj.__dict__ for obj in lx]
+    libraries = [library.to_dict() for library in libraries]
 
-    # Group by request_group_id and count the number of libraries in each group
-    # This is done by creating a dictionary where the keys are the request_group_ids and the values are the counts of libraries
-    logger.debug("Grouping libraries by request_group_id")
-    libraries_per_request_group = {}
-    for library in libraries:
-        request_group_id = library["request_group_id"]
-        if request_group_id in libraries_per_request_group:
-            libraries_per_request_group[request_group_id] += 1
-        else:
-            libraries_per_request_group[request_group_id] = 1
+    # Calculate the total number of libraries
+    total_libraries = len(libraries)
 
-    # Calculate the average number of libraries per request group
-    # This is done by dividing the total number of libraries by the number of request groups
-    # If there are no request groups, the average is set to 0
-    logger.debug("Calculating the average number of libraries per request group")
-    if len(libraries_per_request_group) == 0:
-        average_number_of_libraries_per_request_group = 0
-    else:
-        average_number_of_libraries_per_request_group = round(
-            sum(libraries_per_request_group.values())
-            / len(libraries_per_request_group),
-            1,
-        )
+    # Calculate the total number of requirements
+    total_requirements = sum([len(library['requirements']) for library in libraries])
 
-    # Log the calculated average
-    logger.info(
-        f"The average number of library requests per request group is {average_number_of_libraries_per_request_group}"
-    )
+    # Calculate the average number of libraries per requirement
+    # If there are no requirements, return None
+    average = total_libraries / total_requirements if total_requirements else None
 
-    # Return the calculated average
-    return average_number_of_libraries_per_request_group
+    # Return the average number of libraries per requirement
+    return average
