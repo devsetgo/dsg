07a3bdfc6fb9c89f946eba755e49d70bb39840ab
devsetgo <mikeryan56@gmail.com>
2024-05-26 01:45:39 +0000

    adding code comments

--- src/functions/ai.py
+++ src/functions/ai.py
@@ -1,4 +1,13 @@
 # -*- coding: utf-8 -*-
+"""
+This module contains functions for interacting with the OpenAI API.
+
+It uses the AsyncOpenAI client to make asynchronous requests to the API. The module includes functions for getting tags, summary, mood analysis, and mood from a given content.
+
+Functions:
+    get_analysis(content: str, mood_process: str = None) -> dict: Gets the tags, summary, mood analysis, and mood from the given content.
+
+"""
 import ast
 import re
 
@@ -18,36 +27,68 @@ temperature = 0.2
 
 
 async def get_analysis(content: str, mood_process: str = None) -> dict:
+    """
+    Gets the tags, summary, mood analysis, and mood from the given content.
+
+    Args:
+        content (str): The content to analyze.
+        mood_process (str, optional): The mood process. Defaults to None.
+
+    Returns:
+        dict: A dictionary containing the tags, summary, mood analysis, and mood.
+    """
     logger.info("Starting get_analysis function")
+
+    # Get the tags from the content
     tags = await get_tags(content=content)
+
+    # Get the summary from the content
     summary = await get_summary(content=content, sentence_length=1)
-    mood_analysis = await get_mood_analysis(
-        content=content,
-    )
+
+    # Get the mood analysis from the content
+    mood_analysis = await get_mood_analysis(content=content)
 
     mood = None
+    # If the mood process is not one of the expected values, get the mood from the content
     if mood_process not in ["postive", "negative", "neutral"]:
         mood = await get_mood(content=content)
-        # mood_analysis = mood.get("mood")
         logger.info("processing mood")
+
+    # Create a dictionary with the analysis results
     data = {
         "tags": tags,
         "summary": summary,
         "mood_analysis": mood_analysis["mood_analysis"],
         "mood": mood,
     }
+
     logger.info("openai request completed")
     logger.critical(f"analysis: {data}")
     logger.info("Finished get_analysis function")
+
     return data
 
 
 async def get_tags(
     content: str, temperature: float = temperature, keyword_limit: int = 3
 ) -> dict:
+    """
+    Gets a list of keywords from the given content.
+
+    Args:
+        content (str): The content to analyze.
+        temperature (float, optional): The temperature to use for the OpenAI API. Defaults to temperature.
+        keyword_limit (int, optional): The maximum number of keywords to return. Defaults to 3.
+
+    Returns:
+        dict: A dictionary containing the keywords.
+    """
     logger.info("Starting get_tags function")
+
+    # Create the prompt for the OpenAI API
     prompt = f"For the following text create a python style list between 1 to {keyword_limit} 'one word' keywords to be stored as a python list and cannot be a persons name: {content}"
 
+    # Send the prompt to the OpenAI API
     chat_completion = await client.chat.completions.create(
         model="gpt-3.5-turbo-1106",
         messages=[
@@ -63,6 +104,7 @@ async def get_tags(
     # Extract the content from the response
     response_content = chat_completion.choices[0].message.content
     logger.critical(f"tag response: {response_content}")
+
     # Use a regular expression to extract a list from the response
     match = re.search(r"\[.*\]", response_content)
     if match:
@@ -86,17 +128,33 @@ async def get_tags(
     ]
 
     # Store the keywords in a dictionary
+    logger.debug(f"tag content {response_content}")
     response_dict = {"tags": response_content}
     logger.info("Finished get_tags function")
+
     return response_dict
 
 
 async def get_summary(
     content: str, temperature: float = temperature, sentence_length: int = 1
 ) -> dict:
+    """
+    Gets a summary of the given content.
+
+    Args:
+        content (str): The content to summarize.
+        temperature (float, optional): The temperature to use for the OpenAI API. Defaults to temperature.
+        sentence_length (int, optional): The length of the summary in sentences. Defaults to 1.
+
+    Returns:
+        dict: A dictionary containing the summary.
+    """
     logger.info("Starting get_summary function")
+
+    # Create the prompt for the OpenAI API
     prompt = f"Please summarize the content and provide {sentence_length} concise sentence in length as a title and it cannot contain any persons name"
 
+    # Send the prompt to the OpenAI API
     chat_completion = await client.chat.completions.create(
         model="gpt-3.5-turbo-1106",
         messages=[
@@ -111,18 +169,32 @@ async def get_summary(
 
     # Extract the content from the response
     response_content = chat_completion.choices[0].message.content
-    logger.critical(f"summary content: {response_content}")
+    logger.debug(f"summary content: {response_content}")
+
     # Store the summary in a dictionary
     response_dict = response_content
     logger.info("Finished get_summary function")
+
     return response_dict
 
 
 async def get_mood_analysis(content: str, temperature: float = temperature) -> dict:
+    """
+    Analyzes the mood of the given content.
+
+    Args:
+        content (str): The content to analyze.
+        temperature (float, optional): The temperature to use for the OpenAI API. Defaults to temperature.
+
+    Returns:
+        dict: A dictionary containing the mood analysis.
+    """
     logger.info("Starting get_mood_analysis function")
-    # prompt = f"Please analyze the mood of the following text and provide a single word to describe the content: {content}"
+
+    # Create the prompt for the OpenAI API
     prompt = "For the following text provide a single expressive word response that expresses the general mood of the content. It will be stored in a python variable with a max character length of 25."
 
+    # Send the prompt to the OpenAI API
     chat_completion = await client.chat.completions.create(
         model="gpt-3.5-turbo-1106",
         messages=[
@@ -137,18 +209,34 @@ async def get_mood_analysis(content: str, temperature: float = temperature) -> d
 
     # Extract the content from the response
     response_content = chat_completion.choices[0].message.content
-    logger.critical(f"mood analysis content: {response_content}")
+    logger.debug(f"mood analysis content: {response_content}")
+
     # Store the mood analysis in a dictionary
     response_dict = {"mood_analysis": response_content}
     logger.info("Finished get_mood_analysis function")
+
     return response_dict
 
 
 async def get_mood(content: str, temperature: float = temperature) -> dict:
+    """
+    Determines the mood of the given content.
+
+    Args:
+        content (str): The content to analyze.
+        temperature (float, optional): The temperature to use for the OpenAI API. Defaults to temperature.
+
+    Returns:
+        dict: A dictionary containing the mood.
+    """
+    # Define the possible moods
     moods: list = ["positive", "negative", "neutral"]
     logger.info("Starting get_mood function")
+
+    # Create the prompt for the OpenAI API
     prompt = f"Please determine the mood of the following text using only one of these moods {moods} for the content. It will be stored in a python variable with a max character length of 25."
 
+    # Send the prompt to the OpenAI API
     chat_completion = await client.chat.completions.create(
         model="gpt-3.5-turbo-1106",
         messages=[
@@ -163,27 +251,51 @@ async def get_mood(content: str, temperature: float = temperature) -> dict:
 
     # Extract the content from the response
     response_content = chat_completion.choices[0].message.content
+    logger.debug(f"mood content: {response_content}")
 
     # Store the mood in a dictionary
     response_dict = {"mood": response_content}
-    logger.critical(f"mood content: {response_content}")
+    
     logger.info("Finished get_mood function")
+
     return response_dict
 
 
 async def analyze_post(content: str, temperature: float = temperature) -> dict:
+    """
+    Analyzes a post and returns tags, summary, mood analysis, and mood.
+
+    Args:
+        content (str): The content to analyze.
+        temperature (float, optional): The temperature to use for the OpenAI API. Defaults to temperature.
+
+    Returns:
+        dict: A dictionary containing the tags, summary, mood analysis, and mood.
+    """
     logger.info("Starting analyze_post function")
+
+    # Get the tags from the content
     tags = await get_tags(content=content)
+
+    # Get a two-sentence summary of the content
     summary = await get_summary(content=content, sentence_length=2)
+
+    # Analyze the mood of the content
     mood_analysis = await get_mood_analysis(content=content)
+
+    # Determine the mood of the content
     mood = await get_mood(content=content)
+
+    # Store the analysis results in a dictionary
     data = {
         "tags": tags,
         "summary": summary,
         "mood_analysis": mood_analysis,
         "mood": mood,
     }
+
     logger.info("openai request completed")
     logger.critical(f"post analysis content: {data}")
     logger.info("Finished analyze_post function")
+
     return data
