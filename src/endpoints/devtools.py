719cb28abf2643e4bd43ebae8d237366da8f7e04
devsetgo <mikeryan56@gmail.com>
2024-06-15 23:47:33 +0000

    working on fixing code

--- src/endpoints/devtools.py
+++ src/endpoints/devtools.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from typing import List
-
+import uuid
 from fastapi import APIRouter, Body, HTTPException
 from fastapi.responses import JSONResponse
 from loguru import logger
@@ -13,11 +13,13 @@ router = APIRouter()
 @router.post("/pypi/check-list")
 async def post_check_pypi_packages(packages: List[str] = Body(...)):
     logger.info(f"Checking packages: {packages}")
+    
     try:
-        data = await check_packages(packages)
+        data = await check_packages(packages=packages,request_group_id=str(uuid.uuid4()),request=None)
         logger.info(f"Successfully checked packages: {packages}")
         return JSONResponse(data)
     except Exception as e:
+        print(e)
         logger.error(f"Failed to check packages: {packages}. Error: {e}")
         raise HTTPException(status_code=500, detail="Failed to check packages")
 
@@ -25,11 +27,13 @@ async def post_check_pypi_packages(packages: List[str] = Body(...)):
 @router.get("/pypi/check-one")
 async def get_check_pypi_packages(package: str):
     logger.info(f"Checking package: {package}")
+    print(package)
     try:
         packages = [package]
-        data = await check_packages(packages)
+        data = await check_packages(packages, request=None, request_group_id=str(uuid.uuid4()))
         logger.info(f"Successfully checked package: {package}")
         return JSONResponse(data[0])
     except Exception as e:
+
         logger.error(f"Failed to check package: {package}. Error: {e}")
         raise HTTPException(status_code=500, detail="Failed to check package")
