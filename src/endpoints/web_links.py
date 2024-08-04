5093b93b02a1fdf2ce7394b0a678558cd3d50108
devsetgo <mikeryan56@gmail.com>
2024-08-04 19:39:38 +0000

    fixes for weblink changes

--- src/endpoints/web_links.py
+++ src/endpoints/web_links.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-This module, `web_links.py`, serves as a part of a web application that showcases interesting things, categorized and filterable by tags, date ranges, and categories. It utilizes FastAPI for routing and SQLAlchemy for database interactions, providing an API endpoint to list interesting things with support for pagination.
+This module, `web_links.py`, serves as a part of a web application that showcases interesting weblinks, categorized and filterable by tags, date ranges, and categories. It utilizes FastAPI for routing and SQLAlchemy for database interactions, providing an API endpoint to list interesting weblinks with support for pagination.
 
 Author:
     Mike Ryan
@@ -18,15 +18,15 @@ Dependencies:
     - resources: Provides access to common resources like database operations (`db_ops`) and HTML templates.
 
 API Endpoints:
-    - GET "/": Lists interesting things with optional filters applied. Supports pagination through `offset` and `limit` query parameters.
-    - GET "/categories": Retrieves a list of categories for interesting things.
-    - GET "/pagination": Lists interesting things with pagination and optional filters.
+    - GET "/": Lists weblinks with optional filters applied. Supports pagination through `offset` and `limit` query parameters.
+    - GET "/categories": Retrieves a list of categories for weblinks.
+    - GET "/pagination": Lists weblinks with pagination and optional filters.
     - GET "/new": Displays a form to create a new interesting thing.
     - POST "/new": Creates a new interesting thing based on form data.
 
 
 Usage:
-    This module is designed to be integrated into a FastAPI application, providing a backend API for listing interesting things. It can be used in web applications that require content curation and discovery features, with the ability to filter and paginate through large sets of data.
+    This module is designed to be integrated into a FastAPI application, providing a backend API for listing weblinks. It can be used in web applications that require content curation and discovery features, with the ability to filter and paginate through large sets of data.
 """
 from datetime import datetime
 
@@ -58,15 +58,15 @@ async def list_of_web_links(
         user_timezone = "America/New_York"
 
     query = Select(WebLinks).limit(20).offset(0)
-    things = await db_ops.read_query(query=query)
+    weblinks = await db_ops.read_query(query=query)
 
-    if isinstance(things, str):
-        logger.error(f"Unexpected result from read_query: {things}")
-        things = []
+    if isinstance(weblinks, str):
+        logger.error(f"Unexpected result from read_query: {weblinks}")
+        weblinks = []
     else:
-        things = [thing.to_dict() for thing in things]
+        weblinks = [link.to_dict() for link in weblinks]
     # offset date_created and date_updated to user's timezone
-    for thing in things:
+    for thing in weblinks:
         thing["date_created"] = await date_functions.timezone_update(
             user_timezone=user_timezone,
             date_time=thing["date_created"],
@@ -77,7 +77,7 @@ async def list_of_web_links(
             date_time=thing["date_updated"],
             friendly_string=True,
         )
-    context = {"page": "things", "request": request, "things": things}
+    context = {"page": "weblinks", "request": request, "weblinks": weblinks}
     return templates.TemplateResponse(
         request=request, name="/weblinks/index.html", context=context
     )
@@ -95,14 +95,14 @@ async def get_categories():
 
 
 @router.get("/pagination")
-async def read_things_pagination(
+async def read_weblinks_pagination(
     request: Request,
     search_term: str = Query(None, description="Search term"),
     start_date: str = Query(None, description="Start date"),
     end_date: str = Query(None, description="End date"),
     category: str = Query(None, description="Category"),
     page: int = Query(1, description="Page number"),
-    limit: int = Query(20, description="Number of interesting things per page"),
+    limit: int = Query(20, description="Number of weblinks per page"),
     user_info: dict = Depends(check_login),
 ):
     query_params = {
@@ -147,15 +147,15 @@ async def read_things_pagination(
         .limit(limit)
         .offset(offset)
     )
-    things = await db_ops.read_query(query=query)
-    logger.debug(f"interesting things returned from pagination query {things}")
-    if isinstance(things, str):
-        logger.error(f"Unexpected result from read_query: {things}")
-        things = []
+    weblinks = await db_ops.read_query(query=query)
+    logger.debug(f"weblinks returned from pagination query {weblinks}")
+    if isinstance(weblinks, str):
+        logger.error(f"Unexpected result from read_query: {weblinks}")
+        weblinks = []
     else:
-        things = [thing.to_dict() for thing in things]
+        weblinks = [thing.to_dict() for thing in weblinks]
     # offset date_created and date_updated to user's timezone
-    for thing in things:
+    for thing in weblinks:
         thing["date_created"] = await date_functions.timezone_update(
             user_timezone=user_timezone,
             date_time=thing["date_created"],
@@ -166,12 +166,12 @@ async def read_things_pagination(
             date_time=thing["date_updated"],
             friendly_string=True,
         )
-    found = len(things)
-    things_count = await db_ops.count_query(query=query)
+    found = len(weblinks)
+    weblinks_count = await db_ops.count_query(query=query)
 
     current_count = found
 
-    total_pages = -(-things_count // limit)  # Ceiling division
+    total_pages = -(-weblinks_count // limit)  # Ceiling division
     # Generate the URLs for the previous and next pages
     prev_page_url = (
         f"/weblinks/pagination?page={page - 1}&"
@@ -185,14 +185,14 @@ async def read_things_pagination(
         if page < total_pages
         else None
     )
-    logger.info(f"Found {found} interesting things")
+    logger.info(f"Found {found} weblinks")
     return templates.TemplateResponse(
         request=request,
         name="/weblinks/pagination.html",
-        context={
-            "things": things,
+        context={"page": "weblinks",
+            "weblinks": weblinks,
             "found": found,
-            "note_count": things_count,
+            "note_count": weblinks_count,
             "total_pages": total_pages,
             "current_count": current_count,
             "current_page": page,
@@ -207,7 +207,7 @@ async def new_thing(request: Request):
     return templates.TemplateResponse(
         request=request,
         name="/weblinks/new.html",
-        context={"request": request},
+        context={"page": "weblinks","request": request},
     )
 
 
@@ -246,98 +246,3 @@ async def create_thing(
     return RedirectResponse(url=f"/interesting-thing/view/{data.pkid}", status_code=302)
 
 
-# @router.get("/edit/{post_id}")
-# async def edit_post_form(
-#     request: Request,
-#     post_id: str = Path(...),
-#     user_info: dict = Depends(check_login),
-# ):
-#     user_identifier = user_info["user_identifier"]
-#     user_timezone = user_info["timezone"]
-
-#     query = Select(Posts).where(
-#         and_(Posts.user_id == user_identifier, Posts.pkid == post_id)
-#     )
-#     post = await db_ops.read_one_record(query=query)
-#     if post is None:
-#         logger.warning(f"No post found with ID: {post_id} for user: {user_identifier}")
-#         return RedirectResponse(url="/posts", status_code=302)
-
-#     post = post.to_dict()
-
-#     # offset date_created and date_updated to user's timezone
-#     post["date_created"] = await date_functions.timezone_update(
-#         user_timezone=user_timezone,
-#         date_time=post["date_created"],
-#         friendly_string=True,
-#     )
-#     post["date_updated"] = await date_functions.timezone_update(
-#         user_timezone=user_timezone,
-#         date_time=post["date_updated"],
-#         friendly_string=True,
-#     )
-
-#     logger.info(
-#         f"Returning edit form for post with ID: {post_id} for user: {user_identifier}"
-#     )
-
-#     return templates.TemplateResponse(
-#         request=request,
-#         name="/posts/edit.html",
-#         context={"post": post},  # , "summary": ai.mood_analysis},
-#     )
-
-
-# @router.post("/edit/{post_id}")
-# async def update_post(
-#     background_tasks: BackgroundTasks,
-#     request: Request,
-#     post_id: str = Path(...),
-#     user_info: dict = Depends(check_login),
-# ):
-#     user_info["user_identifier"]
-#     user_info["timezone"]
-
-#     # Fetch the old data
-#     old_data = await db_ops.read_one_record(
-#         query=Select(Posts).where(Posts.pkid == post_id)
-#     )
-#     old_data = old_data.to_dict()
-
-#     # Get the new data from the form
-#     form = await request.form()
-
-#     # Initialize the updated data dictionary with the current date and time
-#     updated_data = {"date_updated": datetime.utcnow()}
-
-#     # List of fields to update
-#     fields = ["mood", "post", "tags", "summary", "mood_analysis"]
-#     # Compare the old data to the new data
-#     for field in fields:
-#         new_value = form.get(field)
-#         if new_value is not None and new_value != old_data.get(field):
-#             if field == "tags":
-#                 # Ensure tags is a list
-#                 if isinstance(new_value, str):
-#                     new_value = [tag.strip() for tag in new_value.split(",")]
-#                 elif not isinstance(new_value, list):
-#                     new_value = [new_value]
-#             updated_data[field] = new_value
-
-#     # Update the database
-#     data = await db_ops.update_one(
-#         table=Posts, record_id=post_id, new_values=updated_data
-#     )
-#     logger.info(f"Updated post with ID: {post_id}")
-#     # background_tasks.add_task(
-#     #     notes_metrics.update_notes_metrics, user_id=user_identifier
-#     # )
-#     return RedirectResponse(url=f"/posts/view/{data.pkid}", status_code=302)
-
-# get /item/{id}
-
-# post (edit) /item/{id}
-
-# post (delete) /delete/{id}
-
-#
